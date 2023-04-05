ca969dd6e9be639cdb21b77587569f663b7a8d5c
Michael Conklin <michaelconklin2004@gmail.com>
2023-04-05 19:39:07 -0400

    added code

--- Develop/script.js
+++ Develop/script.js
@@ -32,5 +32,10 @@ $( document ).ready(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
+  
+  
   // TODO: Add code to display the current date in the header of the page.
-
+  function displayTime() {
+    var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
+  }
